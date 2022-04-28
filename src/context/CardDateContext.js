68534f85d220aadbbdcad409ad3a249ef626c4a2
cks3066 <cks3066@naver.com>
2022-04-28 22:42:48 +0900

    feat: 테스트 커밋

--- src/context/CardDateContext.js
+++ src/context/CardDateContext.js
@@ -8,6 +8,8 @@ const CardDataContext = createContext({
 export const CardsProvider = ({ children }) => {
   const [cards, setCards] = useState([]);
 
+  // value 객체는 객체이므로 리렌더링의 주범이 되므로 useMemo로 캐싱해두세요.
+  // 안 그러면 나중에 이 데이터를 쓰는 모든 컴포넌트가 매번 리렌더링됩니다.
   const value = useMemo(
     () => ({
       cards,
